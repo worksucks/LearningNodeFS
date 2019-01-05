6b2f40104ff9c4be43c32d5947d42d3106f643d9
Tomasz Reizer <t_reizer@yahoo.com>
2019-01-05 18:29:50 +0100

    Eliminate colors module

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-//var fs = require('fs');
-//var StatMode = require('stat-mode');
 
-//fs.stat('./cat.jpg', function(err, stats) {
-//    var statMode = new StatMode(stats);
-//    console.log(statMode.toString());
-//});
 
 
 var fs = require('fs');
-var colors = require('colors');
+
 
 fs.readdir('./node_modules', 'utf-8', (err, files) => {
     files.forEach(file => {
