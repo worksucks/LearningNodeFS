2020e698d5c9ba4442aab3a62bd865cdd2d1a523
Tomasz Reizer <t_reizer@yahoo.com>
2019-01-10 19:18:56 +0100

    index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 
 var fs = require('fs');
 
-
 fs.readdir('./node_modules', 'utf-8', (err, files) => {
     files.forEach(file => {
+      if (err) throw err;
       console.log(file);
       fs.appendFile('./tekst.txt', file , function(err) {
-      if (err) throw err;
+
       });
     });
 });
